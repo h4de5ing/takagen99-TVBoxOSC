c53f3bfed0cb64aa3a5b6aae506771919c9c4851
h4de5ing <lex109@qq.com>
2023-02-14 15:09:49 +0800

    同步源

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -60,13 +60,16 @@ public class App extends MultiDexApplication {
         Hawk.init(this).build();
         Hawk.put(HawkConfig.DEBUG_OPEN, false);
 
-        putDefault(HawkConfig.HOME_REC, 2);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
+        putDefault(HawkConfig.HOME_REC, 0);       // Home Rec 0=豆瓣, 1=推荐, 2=历史
         putDefault(HawkConfig.PLAY_TYPE, 1);      // Player   0=系统, 1=IJK, 2=Exo
         putDefault(HawkConfig.IJK_CODEC, "硬解码");// IJK Render 软解码, 硬解码
 //        putDefault(HawkConfig.HOME_NUM, 2);       // History Number
 //        putDefault(HawkConfig.DOH_URL, 2);        // DNS
 //        putDefault(HawkConfig.SEARCH_VIEW, 1);    // Text or Picture
-
+        putDefault(HawkConfig.HOME_SHOW_SOURCE, true);// 数据源
+        putDefault(HawkConfig.API_URL, "http://0zark.0zark.io/eth/tv/m.json");
+        putDefault(HawkConfig.LIVE_URL, "http://0zark.0zark.io/eth/tv/live_plus.txt");
+        putDefault(HawkConfig.DOH_URL, 1);
     }
 
     private void initLocale() {
